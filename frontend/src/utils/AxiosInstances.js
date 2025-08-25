8bd5d3bcfe65b81ca7b18b1f12da91641d9cd768
Brayan <brayancastillo743@gmail.com>
2025-08-24 19:25:16 -0500

    changes in AxiosInstance

--- frontend/src/utils/AxiosInstances.js
+++ frontend/src/utils/AxiosInstances.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import UserService from '../components/service/UserService';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080", //URL base del backend
+    baseURL: process.env.REACT_APP_API_URL, //URL base del backend
 });
 
 
